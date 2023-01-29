9114b50fea952f9941cb4069f103a2683acf3bac
Yukhimuk Dzmitry <yuhimuk.d@gmail.com>
2023-01-29 20:11:21 +0300

    formic refactor

--- src/components/startMenu/StartMenu.tsx
+++ src/components/startMenu/StartMenu.tsx
@@ -12,19 +12,20 @@ export const StartMenu = () => {
             playerName: '',
             roomNumber: '',
         },
-        validate:(values: {playerName:string, roomNumber:string}) => {
-        const errors:{playerName:string, roomNumber:string} = {
-            playerName: '',
-            roomNumber: ''
-        };
-        if (!values.playerName) {
-            errors.playerName = 'Required';
+        validate: (values: { playerName: string, roomNumber: string }) => {
+            const errors: { playerName: string, roomNumber: string } = {
+                playerName: '',
+                roomNumber: ''
+            };
+            if (!values.playerName) {
+                errors.playerName = 'Required';
+            }
             if (!values.roomNumber) {
                 errors.roomNumber = 'Required';
             }
             return errors;
         }
-    },
+        ,
         onSubmit: values => {
             dispatch(joinGameRoomTC(values.roomNumber))
             dispatch(setPlayerName(values.playerName))
@@ -33,20 +34,20 @@ export const StartMenu = () => {
 
     return (
         <form onSubmit={formik.handleSubmit}>
-        <div className="startGameContainer">
-            <h4>Enter your name</h4>
-            <input className="gameInput"
-                   placeholder="Your name"
-                   {...formik.getFieldProps('playerName')}/>
-            <div className="errorMessage">{formik.errors.playerName}</div>
-            <h4>Enter Room number to Join the Game</h4>
-            <input className="gameInput"
-                   placeholder="Room number"
-                   {...formik.getFieldProps('roomNumber')}/>
-            <div className="errorMessage">{formik.errors.roomNumber}</div>
-            <button type="submit" className="gameButton">Start game</button>
-        </div>
-       </form>
+            <div className="startGameContainer">
+                <h4>Enter your name</h4>
+                <input className="gameInput"
+                       placeholder="Your name"
+                       {...formik.getFieldProps('playerName')}/>
+                <div className="errorMessage">{formik.errors.playerName}</div>
+                <h4>Enter Room number to Join the Game</h4>
+                <input className="gameInput"
+                       placeholder="Room number"
+                       {...formik.getFieldProps('roomNumber')}/>
+                <div className="errorMessage">{formik.errors.roomNumber}</div>
+                <button type="submit" className="gameButton">Start game</button>
+            </div>
+        </form>
     );
 };
 
